ce5dbfc5a8879e52235ac8388ded6d9aec50f980
Philip Hadviger <philip.hadviger@phad.me>
2017-02-23 16:42:10 +0900

    Revert "trying out removing the port, which seems to be the cause of the starphleet problems"
    This reverts commit a594c2c4db4664bdcb00081e1a88b37d67b98418.

--- src/plugins/elasticsearch/lib/expose_client.js
+++ src/plugins/elasticsearch/lib/expose_client.js
@@ -42,6 +42,7 @@ module.exports = function (server) {
 
     const host = {
       host: uri.hostname,
+      port: uri.port,
       protocol: uri.protocol,
       path: uri.pathname,
       auth: uri.auth,
